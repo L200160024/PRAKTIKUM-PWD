ff2580ce3b4652e88a2efe9f0ed05a0cbd9cb49f
L200160024 <43384194+L200160024@users.noreply.github.com>
2018-10-02 22:06:04 +0700

    Add files via upload

---
+++ MODUL 3/tanggal.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>
+			Fungsi Tanggal dan Waktu
+		</title>
+	</head>
+	<body>
+		<?php
+		date_default_timezone_set('Asia/Jakarta');
+		$jam=date("H:i:s A");
+		$waktu=date("d-m-y");
+		$hari=date("l");
+		$tanggal=date("d");
+		$bulan=date("F");
+		$tahun=date("Y");
+		echo "sekarang jam $jam</br>";
+		echo "Sekarang tanggal $waktu</br>";
+		echo "Lebih detailnya hari $hari Tanggal $tanggal Bulan $bulan tahun $tahun";
+		?>
+	</body>
+</html>
